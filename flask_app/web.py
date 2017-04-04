beb7b9708ec8bc96bc67ebfdb70668b078fb0240
emilielimaburke <emilie.burke@gmail.com>
2017-04-04 07:43:47 -0400

    add name parameter

--- flask_app/web.py
+++ flask_app/web.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+	name = request.values.get('name')
+	return render_template('index.html', name=name)
 
 @app.route("/about")
 def about():
