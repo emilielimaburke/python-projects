c426b2dc54a03e2e855cc280a57c81e7bd121912
emilielimaburke <emilie.burke@gmail.com>
2019-09-02 19:24:44 -0400

    ?

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,12 +6,12 @@
 # Tip: % (modulo) tells you what's left over when you divide one number by another
 # ex. number % 3 == 0
 
-for number in range(1,101):
-	if number % 3 == 0 and number % 5 == 0:
-		print("FizzBuzz")
-	elif number % 3 == 0:
-		print("Fizz")
-	elif number % 5 == 0: 
-		print("Buzz")
-	else:
-		print(number)
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
